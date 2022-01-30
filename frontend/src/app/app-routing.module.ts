c84e07f5ef990967ddd649fdb663609376c14f7a
LBCHTS <loicbeauchaints@gmail.com>
2022-01-30 22:56:42 +0100

    refactor: changement css par scss

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './content/about/about.component';
+import { ConceptComponent } from './content/concept/concept.component';
+import { PricesComponent } from './content/prices/prices.component';
+import { SigninComponent } from './content/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'concept',  component: ConceptComponent },
+  { path: 'prices',   component: PricesComponent  },
+  { path: 'about-us', component: AboutComponent   },
+  { path: 'sign-in',  component: SigninComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AppRoutingModule } from './app-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ConceptComponent } from './content/concept/concept.component';
+import { PricesComponent } from './content/prices/prices.component';
+import { AboutComponent } from './content/about/about.component';
+import { SigninComponent } from './content/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ConceptComponent,
+    PricesComponent,
+    AboutComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatSidenavModule,
-    FlexLayoutModule
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
